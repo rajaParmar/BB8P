d4183226931b4be67d5c6511eb40a88450640d0b
rajaPamar <rajaparmar8541@gmail.com>
2017-08-12 23:30:59 +0530

    move added

--- BB.java
+++ BB.java
@@ -14,26 +14,28 @@ class node implements Cloneable{
 	public node(int[][] grid,char child_type,node parent){
 
 		this.grid=grid;
-		this.cost=outplace_count();
+		outplace_count();
 		this.child_type=child_type;
 		this.parent=parent;
+		this.blank=new int[2];
 		//this.blank=blank;
-		this.blank=find_blank();
+		find_blank();
 
 		}
 
-	public int[] find_blank(){
+	public void find_blank(){
 		int i=0,j=0;
 		for (i=0;i<grid.length;i++){
 
 			for (j=0;j<grid[i].length;j++){
 
-				if(grid[i][j]==-1)
-					break;
+				if(grid[i][j]==-1){
+					this.blank=new int[]{i,j};
+					return;}
 					
 			}
 		}
-		return new int[] {i,j};
+		 
 	}
 
 	public node clone(){
@@ -56,7 +58,7 @@ class node implements Cloneable{
 		}	
 	}
 
-	public int outplace_count(){
+	public void outplace_count(){
 		int count=8;
 
 		for(int i=0;i<3;i++){
@@ -65,19 +67,35 @@ class node implements Cloneable{
 					if(this.grid[i][j]!=goal[i][j])
 						count--;
 			}
-		}return count;
+		}this.cost= count;
 
 	}
 
-	public boolean is_valid_pos(int r,int col){
-		if((r < 0 || r>2) || (col>2 || col<2))
+	public static boolean is_valid_pos(int r,int col){
+		if((r < 0 || r>2) || (col>2 || col<0))
 			return false;
 
 		return true;
 
 	}
 
+	public void swap(int c1[],int c2[]){
+		int temp=this.grid[c1[0]][c1[1]];
+		this.grid[c1[0]][c1[1]]=this.grid[c2[0]][c2[1]];
+		this.grid[c2[0]][c2[1]]=temp;
 
+		this.find_blank();
+		outplace_count();
+
+	}
+
+	public static boolean is_valid_child(char child,char parent){
+		if((child=='l' && parent=='r')||(child=='r' && parent=='l'))
+			return false;
+		if((child=='u' && parent=='d')||(child=='d' && parent=='u'))
+			return false;
+		return true;
+	}
 
 }
 
@@ -93,6 +111,66 @@ public class BB{
 
 			node current=new node(init_grid,'x',null);
 
+			
+
+			int row[]={0,0,-1,-1};
+
+			int col[]={-1,-1,0,0};
+
+			char child_type[]={'l','r','u','d'};
+
+			node children[]=new node[4];
+			while(current.cost!=0){
+				current.print_grid();
+				System.out.println((current.cost));
+				for (int i=0;i<4;i++){
+
+					int shift_r=current.blank[0]+row[i];
+					int shift_col=current.blank[1]+col[i];
+					//System.out.println(current.blank[0]+" "+current.blank[1]+" "+shift_r+" "+shift_col);
+					if(node.is_valid_pos(shift_r,shift_col) && node.is_valid_child(child_type[i],current.child_type)){
+						//System.out.println("Entered!");
+						node temp=current.clone();
+						
+						temp.swap(temp.blank,new int[]{shift_r,shift_col});
+
+						children[i]=temp.clone();
+						children[i].parent=current;
+						children[i].child_type=child_type[i];
+
+						temp=null;
+						
+					}
+					else children[i]=null;
+
+					//find best child
+				}
+				int min=current.cost;int index=-1;
+				for (int j=0;j<children.length;j++){
+					// if(children[j]!=null &&  children[j].cost<min ){
+					// 	index=j;
+					// }
+				System.out.println(children[j].child_type);
+				children[j].print_grid();
+
+
+				}
+					
+				if(index==-1)
+				{
+					System.out.println("Solution Not Found!");
+					break;
+				}
+				else{
+
+
+					current=children[index].clone();
+				}
+
+
+				
+
+			}
 
 
 	}
