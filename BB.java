96984cb7c0ed6ff8165d744b69c1f9e9d693480e
rajaPamar <rajaparmar8541@gmail.com>
2017-08-14 20:45:13 +0530

    Outplace heuristic implementation ...still need a better heuristic!

--- BB.java
+++ BB.java
@@ -1,5 +1,5 @@
-class node implements Cloneable{
-	private static int[][] goal={{1,2,3},{4,-1,5},{6,7,8}};
+class node {
+	private static int[][] goal={{1,2,3},{8,-1,4},{7,6,5}};
 
 
 	public int[][] grid;
@@ -23,6 +23,12 @@ class node implements Cloneable{
 
 		}
 
+
+	public void print_node(){
+		this.print_grid();
+		System.out.println(this.cost+" "+this.child_type+" "+this.parent+" "+this.blank[0]+","+this.blank[1]);
+	}
+
 	public void find_blank(){
 		int i=0,j=0;
 		for (i=0;i<grid.length;i++){
@@ -40,7 +46,15 @@ class node implements Cloneable{
 
 	public node clone(){
 		try{
-			return (node)super.clone();
+			int dest[][]=new int[3][3];
+			for(int i=0;i<3;i++){
+				for(int j=0;j<3;j++){
+					dest[i][j]=this.grid[i][j];
+				}
+			}			
+			node temp=new node(dest,this.child_type,this.parent);
+			return temp;
+
 		}
 		catch(Exception e){
 			return null;
@@ -59,13 +73,13 @@ class node implements Cloneable{
 	}
 
 	public void outplace_count(){
-		int count=8;
+		int count=0;
 
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				if(this.grid[i][j]!=-1)
 					if(this.grid[i][j]!=goal[i][j])
-						count--;
+						count++;
 			}
 		}this.cost= count;
 
@@ -107,22 +121,22 @@ public class BB{
 
 	public static void main(String args[]){
 			
-			int init_grid[][]={{1,7,3},{5,-1,4},{6,2,8}};
+			int init_grid[][]={{6,2,3},{1,8,4},{7,-1,5}};
 
 			node current=new node(init_grid,'x',null);
 
 			
 
-			int row[]={0,0,-1,-1};
+			int row[]={0,0,-1,1};
 
-			int col[]={-1,-1,0,0};
+			int col[]={-1,1,0,0};
 
 			char child_type[]={'l','r','u','d'};
 
 			node children[]=new node[4];
 			while(current.cost!=0){
-				current.print_grid();
-				System.out.println((current.cost));
+				//current.print_grid();
+				//System.out.println((current.cost));
 				for (int i=0;i<4;i++){
 
 					int shift_r=current.blank[0]+row[i];
@@ -131,13 +145,16 @@ public class BB{
 					if(node.is_valid_pos(shift_r,shift_col) && node.is_valid_child(child_type[i],current.child_type)){
 						//System.out.println("Entered!");
 						node temp=current.clone();
-						
+						//System.out.print(temp);
+						//System.out.print(current);
+						//temp.print_node();
 						temp.swap(temp.blank,new int[]{shift_r,shift_col});
-
+						//.temp.print_node();
 						children[i]=temp.clone();
+						//current.print_node();
 						children[i].parent=current;
 						children[i].child_type=child_type[i];
-
+						//children[i].print_node();
 						temp=null;
 						
 					}
@@ -147,19 +164,31 @@ public class BB{
 				}
 				int min=current.cost;int index=-1;
 				for (int j=0;j<children.length;j++){
-					// if(children[j]!=null &&  children[j].cost<min ){
-					// 	index=j;
-					// }
-				System.out.println(children[j].child_type);
-				children[j].print_grid();
+					if(children[j]!=null &&  children[j].cost<min ){
+						index=j;
+						 //children[j].print_node();
 
+					}
 
 				}
-					
+
+
+				if(index==-1){
+					for (int j=0;j<children.length;j++){
+					if(children[j]!=null &&  children[j].cost<=min ){
+						index=j;
+						 //children[j].print_node();
+
+					}
+
+				}
+				}
+				current.print_node();	
 				if(index==-1)
 				{
 					System.out.println("Solution Not Found!");
-					break;
+					return;
+					
 				}
 				else{
 
@@ -171,7 +200,14 @@ public class BB{
 				
 
 			}
+			System.out.println("Solution Found!");
+			//current.print_node();
 
+
+			while(current.child_type!='x'){
+				System.out.println(current.child_type);
+				current=current.parent;
+			}
 
 	}
 }
